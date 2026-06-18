e24b767233d023068bf1ea954f98f0a803de6fe1
shailajae <shailajaenjam0@gmail.com>
2026-06-18 21:27:18 +0530

    terraform-vpc-module

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 resource "aws_vpc" "main" {
-  cidr_block           = var.vpc_cidr
+  cidr_block       = var.vpc_cidr
   enable_dns_hostnames = var.enable_dns_hostnames
-  instance_tenancy     = "default"
+  instance_tenancy = "default"
 
   # expense-dev
   tags = merge(
     var.common_tags,
     var.vpc_tags,
     {
-      Name = local.resource_name
+        Name = local.resource_name
     }
   )
 }
@@ -27,7 +27,7 @@ resource "aws_internet_gateway" "main" {
 
 # expense-dev-public-us-east-1a
 resource "aws_subnet" "public" {
-  count      = length(var.public_subnet_cidrs)
+  count = length(var.public_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet_cidrs[count.index]
   availability_zone = local.az_names[count.index]
@@ -60,11 +60,11 @@ resource "aws_subnet" "private" {
 
 # expense-dev-database-us-east-1a
 resource "aws_subnet" "database" {
-  count      = length(var.database_subnet_cidrs)
+  count = length(var.database_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.database_subnet_cidrs[count.index]
   availability_zone = local.az_names[count.index]
-  
+
   tags = merge(
     var.common_tags,
     var.database_subnet_tags,
@@ -76,7 +76,7 @@ resource "aws_subnet" "database" {
 
 
 resource "aws_eip" "nat" {
-  domain = "vpc"
+  domain   = "vpc"
 }
 
 resource "aws_nat_gateway" "example" {
@@ -92,8 +92,8 @@ resource "aws_nat_gateway" "example" {
   )
 
   # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Getway for the vpc.
-  depends_on = [aws_internet_gateway.main]  
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.main]
 }
 
 resource "aws_route_table" "public" {
@@ -103,7 +103,7 @@ resource "aws_route_table" "public" {
     var.common_tags,
     var.public_route_table_tags,
     {
-      Name ="${local.resource_name}-public" 
+      Name = "${local.resource_name}-public"
     }
   )
 }
@@ -115,7 +115,7 @@ resource "aws_route_table" "private" {
     var.common_tags,
     var.private_route_table_tags,
     {
-      Name ="${local.resource_name}-private" 
+      Name = "${local.resource_name}-private"
     }
   )
 }
@@ -127,7 +127,7 @@ resource "aws_route_table" "database" {
     var.common_tags,
     var.database_route_table_tags,
     {
-      Name ="${local.resource_name}-database" 
+      Name = "${local.resource_name}-database"
     }
   )
 }
@@ -135,34 +135,34 @@ resource "aws_route_table" "database" {
 resource "aws_route" "public" {
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.main.id  
+  gateway_id = aws_internet_gateway.main.id
 }
 
 resource "aws_route" "private" {
   route_table_id            = aws_route_table.private.id
   destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_nat_gateway.example.id  
+  nat_gateway_id = aws_nat_gateway.example.id
 }
 
 resource "aws_route" "database" {
   route_table_id            = aws_route_table.database.id
   destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_nat_gateway.example.id  
+  nat_gateway_id = aws_nat_gateway.example.id
 }
 
-resource "aws_route_table_association" "public"{
+resource "aws_route_table_association" "public" {
   count = length(var.public_subnet_cidrs)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route_table_association" "private"{
+resource "aws_route_table_association" "private" {
   count = length(var.private_subnet_cidrs)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
 
-resource "aws_route_table_association" "database"{
+resource "aws_route_table_association" "database" {
   count = length(var.database_subnet_cidrs)
   subnet_id      = aws_subnet.database[count.index].id
   route_table_id = aws_route_table.database.id
